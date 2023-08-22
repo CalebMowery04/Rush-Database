9c6569945c6f46e501c273a052410b48e7954cf2
BaboPhett <yapisaac5@gmail.com>
2023-08-22 15:02:17 -0400

    adds adjusted scores and creates a new csv

--- interview_difficulty_weighter.py
+++ interview_difficulty_weighter.py
@@ -2,52 +2,61 @@ import csv
 
 class InterviewWeighter:
     def __init__(self, interview_results_filename):
-        self.interviewers = {}  # Dictionary to store interviewer scores
-        self.global_sum = 0  # Sum of all interview scores
-        self.global_count = 0  # Total number of interview scores
-        self.interview_results_filename = interview_results_filename  # Store the filename
+        self.interviewers = {}
+        self.global_sum = 0
+        self.global_count = 0
+        self.interview_results_filename = interview_results_filename
 
     def process_interviews(self):
+        processed_rows = []  # Store the processed rows
         with open(self.interview_results_filename, 'r') as file:
             csv_reader = csv.reader(file)
-            header = next(csv_reader)  # Skip the header row
+            header = next(csv_reader)
 
             for row in csv_reader:
                 interviewer = row[2]
                 score = float(row[3])
-                interviewee = row[0]
 
-                # Update global sum and count
                 self.global_sum += score
                 self.global_count += 1
 
-                # Update interviewer's score
                 if interviewer in self.interviewers:
                     self.interviewers[interviewer].append(score)
                 else:
                     self.interviewers[interviewer] = [score]
 
-        # Calculate the global average
+                processed_rows.append(row)
+
         global_avg = self.global_sum / self.global_count
 
-        # Calculate the average for each specific interviewer and subtract the global average
         interviewer_avg_diff = {}
         for interviewer, scores in self.interviewers.items():
             avg = sum(scores) / len(scores)
             avg_diff = avg - global_avg
             interviewer_avg_diff[interviewer] = avg_diff
 
+        adjusted_rows = []
+        for row in processed_rows:
+            score = float(row[3])
+            interviewer = row[2]
+            adjusted_score = score - interviewer_avg_diff[interviewer]
+            row.append(adjusted_score)
+            adjusted_rows.append(row)
+
+        new_filename = "adjusted_interview_results.csv"
+        with open(new_filename, 'w', newline='') as new_file:
+            csv_writer = csv.writer(new_file)
+            csv_writer.writerow(header)
+            csv_writer.writerows(adjusted_rows)
+
         return interviewer_avg_diff
 
-
 # CHANGE THESE =================================================================================
-interview_results_filename = "old_data/fall2022interviews.csv" # Provide the correct path to the CSV file
+interview_results_filename = "old_data/fall2022interviews.csv"
 # ==============================================================================================
 
-# Create an instance of the InterviewWeighter class and process the interviews
 weighter = InterviewWeighter(interview_results_filename)
 interviewer_avg_diff = weighter.process_interviews()
 
-# Print the interviewer average differences
 for interviewer, avg_diff in interviewer_avg_diff.items():
     print(f"Interviewer: {interviewer}, Average Difference: {avg_diff}")
